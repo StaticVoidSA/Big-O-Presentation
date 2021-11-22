b922fab48f3e210ef9adbcaabce571eb1461e6fe
JonathanReverside <jonathanj.jenkins@reverside.co.za>
2021-11-21 22:03:57 -0800

    2021-11-21

--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const controller = require('../controllers/fibonacci');
 
 router.get('/exponential', controller.getExponential);
+
 router.post('/exponential', controller.postExponential);
 
 router.get('/polynomial', controller.getPolynomial);
+
 router.post('/polynomial', controller.postPolynomial);
 
 module.exports = router;
--- routes/min-max.js
+++ routes/min-max.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/min-max');
 
 router.get('/getMinMax', controller.getMinMax);
+
 router.post('/postMinMax', controller.postMinMax);
 
 module.exports = router;
